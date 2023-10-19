cc214504159df68225dd8fcf436c3fcacd7765dc
ammaralialis <ammaralialiwa@gmail.com>
2023-10-18 18:46:53 -0600

    fixed: add correct color to the componets

--- component/paidOrders/index.js
+++ component/paidOrders/index.js
@@ -4,18 +4,18 @@ const d = document;
 const grid = d.querySelector(".grid");
 
 export function PaidOrderComponent(data) {
-  return data.map((componentInfo) => {
+  return data.map((componentInfo, index) => {
     const { icon, paidOrder, title } = componentInfo;
 
     return `
-      <div>
+      <div class="component" data-index="${index}">
         <article>
                 ${icon}
             <ul class="info">
                 <li>
                     ${paidOrder}
                 <li/>
-                <li>
+                <li class="title">
                     ${title}
                 <li/>
             <ul/>
